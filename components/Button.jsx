69081d3ff495f2f7f091b4e1331e0300d3eff16f
gratus00 <sakirabdullayev2805@gmail.com>
2021-12-14 20:03:16 -0500

    initial commit

---
+++ components/Button.jsx
@@ -0,0 +1,7 @@
+import styles from "../styles/button.module.css";
+
+const Button = ({path, children}) => {
+    return <button className={styles.customButton}>finger me</button>
+}
+
+export default Button;
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+import Button from "../components/Button";
+
+const home = () => {
+    return <div>
+        <h1>Hi, I'm a boss</h1>
+        <p>a compsci student </p>
+        <Button></Button>
+    </div>
+};
+
+export default home;
