0f943a26ba6e39ea67adfdce217551662367c7a2
gratus00 <sakirabdullayev2805@gmail.com>
2022-01-05 22:57:38 -0500

    Projects section completed

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -17,20 +17,4 @@ function NavBar(props) {
 };
 
 export default NavBar;
-
-
-// const NavBar = () => {
-//     return (
-//         <nav className={styles.navbar}>
-//             <Link href="/" passHref>
-//                 <a className={styles.logoLink}>SHAKIR ABDULLAYEV</a>
-//             </Link>
-            
-//             <Link to={"index-section"} smooth={true} duration={1000} offset={-50} className="desktop">Home</Link>
-//             <Link to={"about-section"} smooth={true} duration={1000} offset={-50} className="desktop">About</Link>
-//             <Link to={"projects-section"} smooth={true} duration={1000} offset={-50} className="desktop">Projects</Link>
-//             <Link to={"contacts-section"} smooth={true} duration={1000} offset={-50} className="desktop">Contacts</Link>
-//         </nav>
-//         );
-//     };
     
--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -5,40 +5,96 @@ const Projects = () => {
     return (
         <div className={styles.project} id="projects-section">
             <h1>Projects</h1>
+            <span>click on projects to see more! <br /><br /></span>
             <div>
-            <h2>Section 1</h2>
-            <p>bLABfgsljkd hgldkjhgsdklg hsdkljg hsdklh gsdkghsdksdfghsdghsdjsjagjjfgjfjfj
-                sdflkgjs dhklsjh lk sdghsdfkjgsdlgjhsdlkg sdhlgksh
-                dsfgksdhgl sdkjghdslkghsdlkjgh sdl kgjhsaskl ghsdfklghsdflkjghsdkjghsdlkf
-                hgsdfkjgd lkhgsfdkl jhgfadl kjghdlk
-                sklfdjghs fdkjhgakldjg hdlkghsdkgh;aLs
+            <h2> <a href="https://github.com/gratus00/PersonalWebsite" target="_blank">Personal Website</a></h2>
+            <p> During the time I had on my winter break, I decided to explore an area I have 
+                never explored: Web Development. Every cool computer science person I knew had their own personal website, except for me.
+                It seemed to be a pretty low stake project and enabled me to express myself to an extent. 
+                So I decided that I should make one of my own.
+
+                <br /><br />
+
+                To create the website I used <b>React</b> and <b>Next.js</b>. I chose Next.js, because it offloads most of the work
+                to the server and gives clients a pre-rendered page, which enables for faster load 
+                times for anyone that visits the web page. React was chosen because it allows for breaking the website
+                down into individual components, work on them and reuse some of them. 
+
+                <br /><br />
+
+                Click the header to check out the repository!
+
                 </p>
+
+                <img src="/react.png" title="React"/> <img src="/nextjs.png" title = "Next.js"/>
             </div>
             
             
             <div>
-            <h2>Section 2</h2>
-            <p>blabla
-                <br /> <br />
-                <br /> <br />
-            </p>
+                <h2><a href="https://github.com/gratus00/Notes-App" target="_blank">Notes App</a></h2>
+                <p> I created this app as practice for creating Android applications that had
+                    persistent memory through the SharedPreferences interface of Android. 
+                    The app is exactly what its name is: you take notes in it. These notes can then
+                    be referred to in the future, and can be deleted by the user. 
+
+                    <br /><br />
+
+                    Click the header to check out the repository!
+
+                </p>
+                
+                <img src="/android.png" title="Android"/> <img src="/java.png" title = "Java"/>
             </div>
             
             
         
             <div>
-            <h2>Section 3</h2>
-            <p>blabla
+            <h2><a href="https://github.com/gratus00/Location-Saver" target="_blank">Location Saver</a></h2>
+            <p> Most people today have a device in their pocket that can connect 
+                to satellites in space that by cooperating can give the person their location <b>anywhere in the world  </b>
+                with an <a href="https://www.gps.gov/systems/gps/performance/accuracy/#how-accurate" target="_blank" style={{textDecoration: 'underline'}}>accuracy of a 5 meter radius. </a> 
+                That is one of the coolest thing that humans have achieved in history.
                 <br /> <br />
+                You know what's cooler? I made an app that uses that same technology to enable a user to save locations
+                in that same pocket device.
                 <br /> <br />
+                To be more descriptive, I created an Android app that utilizes Google Maps API and Android's GPS libraries,
+                to enable users to save a location on their phone by simply pressing and holding a location on a map. 
+                <br /> <br />
+                If there also happens to be an address allocated to that location in the Google Maps API, that address saved as the name of that location. 
+                <br /><br />
+                Feel free to click the header to check out the repository!
+
             </p>
+            <img src="/googlemaps.png" title="Google Maps"/> <img src="/android.png" title="Android"/> <img src="/java.png" title = "Java"/>
             </div>
         
 
             <div>
-            <h2>Section 4</h2>
-            <p>blabla
+            <h2> <a href="https://github.com/gratus00/Your-Location-Snapshot" target="_blank">Your Location Snapshot</a> </h2>
+            <p> This app was created with a certain scenario in mind: imagine you are stuck in a place
+                that you are not familiar with. You have low battery and you must give precise location information to your family 
+                or friends that are searching for you. 
+                <br /><br />
+                Well, with <b>Your Location Snapshot</b>, you can open the app that has the following information:
+                <ul>
+                    <li>Latitude</li>
+                    <li>Longitude</li>
+                    <li>Accuracy of GPS data</li>
+                    <li>Altitude</li>
+                    <li>Address (if it exists).</li>
+                </ul> 
+
+                and just take a screenshot. It is fast, accessible, does not cost large battery power,
+                and does not require any taps on a screen. 
+                
+                <br /><br />
+                Open. Screenshot. 
+                <br /><br />
+                <br /><br />
+                Feel free to click the header to check out the repository!
             </p>
+            <img src="/googlemaps.png" title="Google Maps"/> <img src="/android.png" title="Android"/> <img src="/java.png" title = "Java"/>
             </div>
             
         </div>
