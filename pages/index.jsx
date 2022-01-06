c9ca648fe4c26002e087ef0cc89b02408a99e7e4
gratus00 <sakirabdullayev2805@gmail.com>
2022-01-06 01:47:26 -0500

    Head Title fixed

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,9 +6,15 @@ import About from "./About";
 import Projects from "./Projects";
 import Contacts from "./Contacts";
 import homeStyle from "../styles/home.module.css";
+import Head from "next/head";
 
 const home = () => {
     return <div id="index-section">
+            <div>
+            <Head>
+                <title>Shakir Abdullayev</title>
+            </Head>
+            </div>
                 <NavBar/>
                 <SideBar/>
                 <Background/>
