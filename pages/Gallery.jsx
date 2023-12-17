cb92dd80f2993591e8b2a82c8a2529f990ec97a1
gratus00 <sakirabdullayev2805@gmail.com>
2023-12-17 01:27:28 -0500

    desperately trying to use the nextjs image tag

--- pages/Gallery.jsx
+++ pages/Gallery.jsx
@@ -2,17 +2,26 @@
 import fs from 'fs';
 import path from 'path';
 import styles from "../styles/gallery.module.css";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
+import Image from 'next/image';
+import imageSize from 'image-size';
 
-const Modal = ({ imgSrc, alt, onClose }) => {
+const Modal = ({ imgSrc, alt, onClose, width, height }) => {
     if (!imgSrc) return null;
 
     return (
         <div className={styles.modalOverlay} onClick={onClose}>
             <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
-                <img className={styles.modalImage} src={imgSrc} alt={alt} />
+                <Image 
+                    className={styles.modalImage} 
+                    src={imgSrc} 
+                    alt={alt} 
+                    width={width} 
+                    height={height}
+                    layout="responsive"
+                />
             </div>
         </div>
     );
@@ -20,6 +29,43 @@ const Modal = ({ imgSrc, alt, onClose }) => {
 
 const Gallery = ({ imagePaths }) => {
     const [focusedImg, setFocusedImg] = useState(null);
+    const [modalImgDimensions, setModalImgDimensions] = useState({ width: 0, height: 0 });
+    const [visibleImages, setVisibleImages] = useState([]);
+    const [lastImageIndex, setLastImageIndex] = useState(0);
+    const imagesPerLoad = 10; // Number of images to load per "batch"
+
+    useEffect(() => {
+        // Function to load more images
+        function loadMoreImages() {
+            const newLastIndex = Math.min(lastImageIndex + imagesPerLoad, imagePaths.length);
+            const newImages = imagePaths.slice(lastImageIndex, newLastIndex);
+            setVisibleImages(visibleImages.concat(newImages));
+            setLastImageIndex(newLastIndex);
+        }
+
+        // Initial load
+        loadMoreImages();
+
+        // Event listener for scroll event
+        function handleScroll() {
+            // Check if the user has scrolled to the bottom
+            if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
+                loadMoreImages();
+            }
+        }
+
+        // Register scroll event listener
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup function to remove event listener
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [lastImageIndex, imagePaths]); // Dependencies array
+
+
+    const handleImageClick = (imgPath, width, height) => {
+        setFocusedImg(imgPath);
+        setModalImgDimensions({ width, height });
+    };
 
     return (
         <>
@@ -31,13 +77,15 @@ const Gallery = ({ imagePaths }) => {
             </header>
             
             <div className={styles.masonry}>
-                {imagePaths.map((imgPath, index) => (
-                    <div key={`image-${index}-${imgPath}`} className={styles.gridItem}>
-                        <img
+                {imagePaths.map((image, index) => (
+                    <div key={`image-${index}-${image}`} className={styles.gridItem}>
+                        <Image
                             className={styles.image}
-                            src={`/images/${imgPath}`}
+                            src={`/images/${image.fileName}`}
                             alt={`Image ${index}`}
-                            onClick={() => setFocusedImg(`/images/${imgPath}`)}
+                            width={image.width} 
+                            height={image.height}  
+                            onClick={() => handleImageClick(`/images/${image.fileName}`, image.width, image.height)}
                         />
                     </div>
                 ))}
@@ -46,6 +94,8 @@ const Gallery = ({ imagePaths }) => {
                 imgSrc={focusedImg}
                 alt="Focused Image"
                 onClose={() => setFocusedImg(null)}
+                width={modalImgDimensions.width}
+                height={modalImgDimensions.height}
             />
         </>
     );
@@ -55,9 +105,17 @@ export async function getStaticProps() {
     const imagesDirectory = path.join(process.cwd(), 'public/images');
     const imageFiles = fs.readdirSync(imagesDirectory);
 
+    const imagesWithDimensions = imageFiles.map((fileName) => {
+        const dimensions = imageSize(path.join(imagesDirectory, fileName));
+        return {
+          fileName,
+          ...dimensions,
+        };
+      });
+
     return {
         props: {
-            imagePaths: imageFiles,
+            imagePaths: imagesWithDimensions,
         },
     };
 }
